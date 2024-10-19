362740d83efba886162b40f6c1ba9643d400e545
Pedro Thales Sousa Pereira <138536507+PedroThalesSP@users.noreply.github.com>
2024-10-19 19:49:12 -0300

    Add files via upload

--- My_Version/script.js
+++ My_Version/script.js
@@ -5,6 +5,9 @@ let height_personage = 52;
 let image_hight = 2;
 let image_left = 1;
 
+
+
+
 function sprite(x,y,ctx){
 this.x = x;
 this.t = y;
@@ -18,35 +21,55 @@ this.started_direction = 0;
 this.direction_image = 0;
 }
 
+
+
+// inicio da codificação
 this.half_steps = function(){
 let internal_position_cell = ((this.stage_steps%4) + 1) / 4 * size_cell;
 switch (this.started_direction){
     case left: this.x = this.oldx + internal_position_cell; this.direction_image = image_hight; break;
     case right: this.x = this.oldx - internal_position_cell; this.direction_image = image_left; break;
-}
-
-
-
-}
-
-
-
-
-
-
-this.semiPasso = function()
-{
-  var posicaoInterna_Celula = ((this.etapaPasso%4)+1)/4 * TAMANHO_CELULA;
-
-  switch (this.iniciouDirecao)
-  {
-    case ESQUERDA: this.x = this.oldx + posicaoInterna_Celula;this.direcaoImagem = IMAGEM_DIREITA; break;
-    case DIREITA: this.x = this.oldx - posicaoInterna_Celula;this.direcaoImagem = IMAGEM_ESQUERDA; break;
     default:break;
-  }
-  this.etapaPasso = (this.etapaPasso+1) % 4;
-  if (this.etapaPasso == 0 )
-  {
-    this.iniciouPasso = false;
-  }
 }
+
+this.stage_steps = (this.stage_steps + 1) % 4;
+if (this.stage_steps == 0) {
+    this.stage_steps = false;
+}
+}
+
+
+
+//terminar de codificar
+this.draw = function (){
+    this.ctx.drawImage(this.image, this.stage_steps * size_cell, this.direction_image * height_personage, size_cell, height_personage, this.x, this.y + 10, size_cell, height_personage);
+}
+
+
+// this.passo = function(direcao)
+// {
+//   if (!this.iniciouPasso)
+//   {
+//     var tempx = -1;
+//     var tempy = -1;
+//     this.iniciouPasso=true;
+//     this.iniciouDirecao = direcao;
+//     this.oldx=this.x;
+//     this.oldy=this.y;
+//   }
+// }
+// this.setaMovimento = function(dx,dy)
+// {
+//     this.dx = dx;
+//     this.dy = dy;
+// }
+// }
+// sprite.prototype.ciclo = function()
+// {
+//   if (!this.ativo)
+//     return false;
+//   this.x = this.x+this.dx;
+//   this.y = this.y+this.dy;
+//   return true;
+// }
+
